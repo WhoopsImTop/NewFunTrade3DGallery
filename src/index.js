a22e41c48b8d9dc2f784d05e33700f22a4ee9bae
Elias4711 <elias.englen@geops.de>
2022-03-30 17:28:29 +0200

    add lastest

--- src/index.js
+++ src/index.js
@@ -1,210 +1,258 @@
-import "./style/main.css";
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { Card } from "./classes/card/card.js";
-import gsap from "gsap";
-import { Interaction } from "three.interaction";
+import './style/main.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { Card } from './classes/card/card.js'
+import gsap from 'gsap'
+import { Interaction } from 'three.interaction'
 import Stats from 'stats.js'
 
 //Monitoring
-const stats = new Stats();
-stats.showPanel(0);
-document.body.appendChild(stats.dom);
+const stats = new Stats()
+stats.showPanel(0)
+document.body.appendChild(stats.dom)
 
-const filePaths = require("../static/textures/imagePaths.json");
-let selected;
-let cards = [];
-let progressbar = document.getElementById("progressbar");
+const filePaths = require('../static/textures/imagePaths.json')
+let selected
+let cards = []
+let progressbar = document.getElementById('progressbar')
 
-document.getElementById("preloadButton").addEventListener("click", () => {
-    for (let i = 0; i < filePaths.length;) {
-        const card = new Card(filePaths[i].front, filePaths[i].back);
-        const geometry = card.createGeometry();
-        //change the card position
-        geometry.position.x = Math.random() * 500 - 250;
-        geometry.position.y = Math.random() * 200 - 100;
-        geometry.position.z = Math.random() * 1000 - 500;
-        cards.push(geometry);
-        i++;
-    }
-    setTimeout(() => {
-        progressbar.value = 30;
-    }, 1000);
-    setTimeout(() => {
-        progressbar.value = 80;
-    }, 2000);
-    setTimeout(() => {
-        progressbar.value = 100;
-        runThreeJS();
-    }, 5000);
-});
+document.getElementById('preloadButton').addEventListener('click', () => {
+  for (let i = 0; i < filePaths.length; ) {
+    const card = new Card(filePaths[i].front, filePaths[i].back)
+    const geometry = card.createGeometry()
+    //change the card position
+    geometry.position.x = Math.random() * 500 - 250
+    geometry.position.y = Math.random() * 200 - 100
+    geometry.position.z = Math.random() * 1000 - 500
+    // create a grid of cards
+    /* geometry.position.x = (i % 8) * 20 - 10
+    geometry.position.y = Math.floor(i / 8) * 20 - 10
+    geometry.position.z = 0 */
+    cards.push(geometry)
+    i++
+  }
+  setTimeout(() => {
+    progressbar.value = 30
+  }, 1000)
+  setTimeout(() => {
+    progressbar.value = 80
+  }, 2000)
+  setTimeout(() => {
+    progressbar.value = 100
+    runThreeJS()
+  }, 5000)
+})
 
 function runThreeJS() {
-    document.querySelector('.textContianer').innerHTML = "";
-    document.querySelector(".action-container").style.display = "flex";
-    // generate HTML Canvas
-    let htmlCanvas = document.createElement("canvas");
-    htmlCanvas.classList.add("webgl");
-    let body = document.querySelector("body");
-    body.appendChild(htmlCanvas);
-    // Canvas
-    const canvas = document.querySelector("canvas.webgl");
+  document.querySelector('.textContianer').innerHTML = ''
+  document.querySelector('.action-container').style.display = 'flex'
+  // generate HTML Canvas
+  let htmlCanvas = document.createElement('canvas')
+  htmlCanvas.classList.add('webgl')
+  let body = document.querySelector('body')
+  body.appendChild(htmlCanvas)
+  // Canvas
+  const canvas = document.querySelector('canvas.webgl')
 
-    // Scene
-    const scene = new THREE.Scene();
+  // Scene
+  const scene = new THREE.Scene()
 
-    /**
-     * Object
-     */
-    for (let i = 0; i < cards.length; i++) {
-        cards[i].on("click", () => {
-            selected = cards[i];
-            updateCamera(cards[i]);
-        });
-        cards[i].on("mouseover", () => {
-            document.body.style.cursor = "pointer";
-        });
-        cards[i].on("mouseout", () => {
-            document.body.style.cursor = "default";
-        });
-        scene.add(cards[i]);
+  /**
+   * Object
+   */
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].on('click', () => {
+      selected = cards[i]
+      updateCamera(cards[i])
+    })
+    cards[i].on('mouseover', () => {
+      document.body.style.cursor = 'pointer'
+    })
+    cards[i].on('mouseout', () => {
+      document.body.style.cursor = 'default'
+    })
+    scene.add(cards[i])
+  }
+  /**
+   * Sizes
+   */
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  }
+
+  // light
+  const areaLight = new THREE.AmbientLight(0xffffff, 1)
+  areaLight.position.set(0, 50, 10)
+  scene.add(areaLight)
+
+  // Base camera
+  const camera = new THREE.PerspectiveCamera(
+    35,
+    sizes.width / sizes.height,
+    0.001,
+    200,
+  )
+  camera.position.x = 0
+  camera.position.y = 0
+  camera.position.z = 50
+  scene.add(camera)
+
+  // Controls
+  const controls = new OrbitControls(camera, canvas)
+  controls.enableDamping = true
+  controls.enablePan = true
+  controls.dampingFactor = 0.05
+  controls.maxDistance = 1000
+  controls.minDistance = 30
+  controls.touches = {
+    ONE: THREE.TOUCH.ROTATE,
+    TWO: THREE.TOUCH.DOLLY_PAN,
+  }
+  /**
+   * Renderer
+   */
+
+  let index = 0
+
+  document.querySelector('.btn.minus').addEventListener('click', () => {
+    index--
+    if (index < 0) {
+      index = filePaths.length
     }
-    /**
-     * Sizes
-     */
-    const sizes = {
-        width: window.innerWidth,
-        height: window.innerHeight,
-    };
+    selected = scene.children[index]
+    updateCamera(selected)
+  })
 
-    // light
-    const areaLight = new THREE.AmbientLight(0xffffff, 1);
-    areaLight.position.set(0, 50, 10);
-    scene.add(areaLight);
-
-    // Base camera
-    const camera = new THREE.PerspectiveCamera(
-        35,
-        sizes.width / sizes.height,
-        0.001,
-        200
-    );
-    camera.position.x = 0;
-    camera.position.y = 0;
-    camera.position.z = 50;
-    scene.add(camera);
-
-    // Controls
-    const controls = new OrbitControls(camera, canvas);
-    controls.enableDamping = true;
-    controls.enablePan = true;
-    controls.dampingFactor = 0.05;
-    controls.maxDistance = 1000;
-    controls.minDistance = 30;
-    controls.touches = {
-        ONE: THREE.TOUCH.ROTATE,
-        TWO: THREE.TOUCH.DOLLY_PAN,
-    };
-    /**
-     * Renderer
-     */
-
-    let index = 0;
-
-    document.querySelector(".btn.minus").addEventListener("click", () => {
-        index--;
-        if (index < 0) {
-            index = filePaths.length;
-        }
-        selected = scene.children[index];
-        updateCamera(selected);
-    });
-
-    document.querySelector(".btn.plus").addEventListener("click", () => {
-        index++;
-        if (index == filePaths.length) {
-            index = 0;
-        }
-        selected = scene.children[index];
-        updateCamera(selected);
-    });
-
-    const renderer = new THREE.WebGLRenderer({
-        canvas: canvas,
-        antialias: true,
-    });
-
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-    renderer.setClearColor(0x0ca5e6, 1); //0x0ca5e6
-
-    const interaction = new Interaction(renderer, scene, camera);
-
-    /**
-     * Animate
-     */
-    const clock = new THREE.Clock();
-    const tick = () => {
-        stats.begin()
-        const elapsedTime = clock.getElapsedTime();
-
-        if (selected) {
-            selected.rotation.y += 0.002;
-            selected.position.y += Math.cos(elapsedTime) * 0.01;
-            if (selected.children[0].material.opacity < 0.9) {
-                selected.children[0].material.opacity += 0.01;
-            }
-        }
-        // Update controls
-        controls.update();
-        // Render
-        renderer.render(scene, camera);
-
-        // Call tick again on the next frame
-        window.requestAnimationFrame(tick);
-        stats.end()
-    };
-
-    tick();
-
-    window.addEventListener("resize", () => {
-        // Update sizes
-        sizes.width = window.innerWidth;
-        sizes.height = window.innerHeight;
-
-        // Update camera
-        camera.aspect = sizes.width / sizes.height;
-        camera.updateProjectionMatrix();
-
-        // Update renderer
-        renderer.setSize(sizes.width, sizes.height);
-        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-    });
-
-    function updateCamera(selected) {
-        gsap.to(controls.target, {
-            x: selected.position.x,
-            y: selected.position.y,
-            z: selected.position.z,
-        });
-        gsap.to(camera.position, {
-            duration: 2,
-            delay: 0,
-            x: selected.position.x,
-            y: selected.position.y,
-            z: selected.position.z + 50,
-            ease: "power4.easeOut",
-        });
-
-        camera.lookAt(selected.position);
-
-        for (let i = 0; i < filePaths.length; i++) {
-            scene.children[i].material[4].map.image.pause();
-            scene.children[i].material[5].map.image.pause();
-            scene.children[i].children[0].material.visible = false;
-        }
-        selected.material[4].map.image.play();
-        selected.material[5].map.image.play();
-        selected.children[0].material.visible = true;
+  document.querySelector('.btn.plus').addEventListener('click', () => {
+    index++
+    if (index == filePaths.length) {
+      index = 0
     }
+    selected = scene.children[index]
+    updateCamera(selected)
+  })
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+  })
+
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  renderer.setClearColor(0x0ca5e6, 1) //0x0ca5e6
+
+  const interaction = new Interaction(renderer, scene, camera)
+
+
+  //Add Event Listener
+  // Listen for scroll and move y position of camera to simulate perspective
+    window.addEventListener('wheel', onMouseWheel)
+    let y = 0;
+    let position = 0;
+    function onMouseWheel(e) {
+        y = e.deltaY * 0.001
+    }
+
+  /**
+   * Animate
+   */
+  const clock = new THREE.Clock()
+  const tick = () => {
+    stats.begin()
+    const elapsedTime = clock.getElapsedTime()
+
+    // update camera position
+    position += y;
+    y *= .9;
+    camera.position.y += position
+
+    if (selected) {
+      selected.rotation.y += 0.002
+      selected.position.y += Math.cos(elapsedTime) * 0.01
+      if (selected.children[0].material.opacity < 0.9) {
+        selected.children[0].material.opacity += 0.01
+      }
+    }
+    // Update controls
+    controls.update()
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+    stats.end()
+  }
+
+  tick()
+
+  window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  })
+
+  function updateCamera(selected) {
+    document.querySelector('.textContianer').innerHTML = ''
+    gsap.to(controls.target, {
+      x: selected.position.x + 10,
+      y: selected.position.y,
+      z: selected.position.z,
+    })
+    gsap.to(camera.position, {
+      duration: 2,
+      delay: 0,
+      x: selected.position.x,
+      y: selected.position.y,
+      z: selected.position.z + 50,
+      ease: 'power4.easeOut',
+    })
+
+    camera.lookAt(selected.position)
+
+    for (let i = 0; i < filePaths.length; i++) {
+      scene.children[i].material[4].map.image.pause()
+      scene.children[i].material[5].map.image.pause()
+      scene.children[i].children[0].material.visible = false
+    }
+    selected.material[4].map.image.play()
+    selected.material[5].map.image.play()
+    selected.children[0].material.visible = true
+
+    let infoContainer = document.createElement('div')
+    infoContainer.classList.add('info-container')
+
+    let title = document.createElement('h1')
+    title.innerHTML = filePaths[index].title
+
+    let artist = document.createElement('span')
+    // first character upper case
+    artist.innerHTML =
+      '~ ' +
+      filePaths[index].artist.charAt(0).toUpperCase() +
+      filePaths[index].artist.slice(1)
+
+    let button = document.createElement('button')
+    button.classList.add('btn', 'btn-primary', 'rounded')
+    button.style.marginTop = '50px'
+    button.innerHTML = `<img src="./images/Ethereum_logo.svg" width="20" style="margin-right: 10px;"> Available on OpenSea`
+    button.addEventListener('click', () => {
+      window.open(filePaths[index].link)
+    })
+
+    setTimeout(() => {
+      infoContainer.appendChild(title)
+      infoContainer.appendChild(artist)
+      infoContainer.appendChild(button)
+      document.querySelector('.textContianer').appendChild(infoContainer)
+    }, 2200)
+  }
 }
--- writeImagePathToJson.js
+++ writeImagePathToJson.js
@@ -1,28 +1,37 @@
-let fs = require('fs');
+let fs = require('fs')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const { serialize } = require('v8')
 
-function writeImagePathToJSONFile() {
-    let dir = 'static/textures/catchyTuneRadio/';
-    let file = 'imagePaths.json';
-    
-    fs.readdir(dir, (err, files) => {
-        if (err) {
-            console.log(err);
-        } else {
-            let imagePaths = [];
-            files.forEach(file => {
-                imagePaths.push({
-                    front: 'textures/catchyTuneRadio/' + file,
-                    back: 'textures/catchyTuneRadio/' + '_backside-cover-catchy-tune-radio-ctr-as-01-nft(1).mp4'
-                });
-            });
-            fs.writeFile(file, JSON.stringify(imagePaths, null, 2), (err) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log('File written successfully');
-                }
-            });
+async function writeImagePathToJSONFile() {
+  let dir = 'static/textures/catchyTuneRadio/'
+  let file = 'imagePaths.json'
+
+  await fs.readdir(dir, (err, files) => {
+    if (err) {
+      console.log(err)
+    } else {
+      let imagePaths = []
+      files.forEach(async (file) => {
+        let searchParam = file.split('-')[0].replace(/[aeiouAEIOZ]/g, '_')
+        if (searchParam != '__') {
+          let link =
+            'https://opensea.io/NewFunTradeGallery?search[sortBy]=LISTING_DATE&search[query]=' +
+            searchParam
+          imagePaths.push({
+            link: link,
+          })
         }
-    });
+        file++
+      })
+      fs.writeFile(file, JSON.stringify(imagePaths, null, 2), (err) => {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log('File written successfully')
+        }
+      })
+    }
+  })
 }
-writeImagePathToJSONFile();
+writeImagePathToJSONFile()
